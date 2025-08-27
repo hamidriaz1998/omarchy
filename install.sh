e7c8c51745034de7415b2a4ec74fad00bdfc69c2
David Heinemeier Hansson <david@hey.com>
2025-08-27 10:54:23 +0200

    Don't update system packages to prevent issues from something too new at the wrong time

--- install.sh
+++ install.sh
@@ -54,5 +54,19 @@ source $OMARCHY_INSTALL/login/plymouth.sh
 source $OMARCHY_INSTALL/login/limine-snapper.sh
 source $OMARCHY_INSTALL/login/alt-bootloaders.sh
 
-# Finishing
-source $OMARCHY_INSTALL/reboot.sh
+# Updates
+sudo updatedb
+
+# Reboot
+clear
+tte -i ~/.local/share/omarchy/logo.txt --frame-rate 920 laseretch
+echo
+echo "You're done! So we're ready to reboot now..." | tte --frame-rate 640 wipe
+
+if sudo test -f /etc/sudoers.d/99-omarchy-installer; then
+  sudo rm -f /etc/sudoers.d/99-omarchy-installer &>/dev/null
+  echo -e "\nRemember to remove USB installer!\n\n"
+fi
+
+sleep 5
+reboot
